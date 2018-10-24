fa0f35609a608e7f49156126eaf811f0a94c9c43
Qasim Laghari <go.jets.laghari@gmail.com>
2018-10-23 21:03:04 -0500

    Add solution for reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,20 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  var checkSign = Math.sign(n);
+  var reverse = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  if (checkSign == 1 || checkSign == 0) {
+    return parseInt(reverse);
+  } else {
+    var revNeg = reverse.substring(0, reverse.length - 1);
+    return parseInt(revNeg) - parseInt(revNeg) * 2;
+  }
+}
 
 module.exports = reverseInt;
