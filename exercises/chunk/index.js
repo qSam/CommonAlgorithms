6b0039a1d9e6006ce30f94e70ea3701e563629e1
Qasim Laghari <go.jets.laghari@gmail.com>
2018-10-25 21:20:34 -0500

    Add a second solution for creating a chunk of an array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -11,13 +11,10 @@
 function chunk(array, size) {
   let chunked = [];
 
-  for (let element of array) {
-    let last = chunked[chunked.length - 1];
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index = index + size;
   }
   return chunked;
 }
