958c23bf628c188685bf5d206eb8e7d2e8ff2ad5
Qasim Laghari <go.jets.laghari@gmail.com>
2018-11-04 17:09:47 -0600

    Insert a new head node to linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,11 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
 }
 
 module.exports = { Node, LinkedList };
