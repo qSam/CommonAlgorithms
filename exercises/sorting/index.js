2ab6a950c1b22d3088187fad933232bf4d02a5bf
Qasim Laghari <go.jets.laghari@gmail.com>
2018-11-07 13:27:22 -0600

    Implement solution for selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,7 +14,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (i != minIndex) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
 
 function mergeSort(arr) {}
 
