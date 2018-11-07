9ed62c069722d702ad1833446037bb56700c16dc
Qasim Laghari <go.jets.laghari@gmail.com>
2018-11-07 13:42:45 -0600

    Setup merge function for MergSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -33,6 +33,16 @@ function selectionSort(arr) {
 
 function mergeSort(arr) {}
 
-function merge(left, right) {}
+function merge(left, right) {
+  let results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
