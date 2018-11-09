35193a33c5233add36abf7d09f0463acf66cf7cf
Qasim Laghari <go.jets.laghari@gmail.com>
2018-11-08 22:27:48 -0600

    Do a bread first search

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root, 's'];
+  let counter = [0];
+
+  while (arr.length > 1) {
+    let node = arr.shift();
+
+    if (node === 's') {
+      counter.push(0);
+      arr.push('s');
+    } else {
+      for (let child of node.children) {
+        arr.push(child);
+      }
+      counter[counter.length - 1]++;
+    }
+  }
+  return counter;
+}
 
 module.exports = levelWidth;
